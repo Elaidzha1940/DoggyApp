9bef84c202cc189488152ae28785771d1942a028
Elaidzha1940 <eli080293@gmail.com>
2023-08-22 16:35:11 +0300

    DoggyApp

---
+++ DoggyApp/DoggyAppApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: DoggyApp
+//  File: DoggyAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct DoggyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
