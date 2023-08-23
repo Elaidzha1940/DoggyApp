850928c96bfdce1a0e25f70f1fddc7c74fe2a551
Elaidzha1940 <eli080293@gmail.com>
2023-08-23 13:50:28 +0300

    added CardView

---
+++ DoggyApp/CardVeiw.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: DoggyApp
+//  File: CardVeiw.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 23.08.2023
+// 
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct CardVeiw: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardVeiw_Previews: PreviewProvider {
+    static var previews: some View {
+        CardVeiw()
+    }
+}
